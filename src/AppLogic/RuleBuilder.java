f8ac4019cbc2ac50b6f93f9c82f1f1ace5be6f67
precipitevolissimevolmente <necunoscut23@gmail.com>
2014-03-26 18:17:43 +0200

    Message Completed Regula 2 si aia de jurisdictie

--- src/AppLogic/RuleBuilder.java
+++ src/AppLogic/RuleBuilder.java
@@ -5,11 +5,9 @@ import ban.ActionType;
 import ban.Rule;
 import message.*;
 
-import static ban.ActionType.BELIEVES;
-import static ban.ActionType.SAID;
-import static ban.ActionType.SEES;
+import static ban.ActionType.*;
 import static message.BanObjectType.*;
-import static message.KeyType.SHARED_KEY;
+import static message.KeyType.*;
 
 public class RuleBuilder {
 
@@ -20,9 +18,22 @@ public class RuleBuilder {
 
         if (rule1.getLeft().equals(rule2.getLeft())) {
             if (rule1.getAction().equals(BELIEVES) && rule2.getAction().equals(SEES)) {
-                if ((rule1.getRight().getType().equals(BanObjectType.KEY)) && ((Key) (rule1.getRight())).getKeyType().equals(SHARED_KEY)) {
-                    if ((((Key) (rule1.getRight())).getQ()).equals(((Principal) rule1.getLeft()))) {
-                        //return new Rule();
+                if(rule2.getRight().getType().equals(BanObjectType.ENCRYPTED_MESSAGE)){
+                    if ((rule1.getRight().getType().equals(BanObjectType.KEY))
+                            &&((Key) (rule1.getRight())).getKeyType().equals(SHARED_KEY)
+                            && (((EncryptedMessage) (rule2.getRight())).getKey().getKeyType().equals(SHARED_KEY))
+                            )
+                    {
+                        if ((((Key) (rule1.getRight())).getQ()).equals(rule1.getLeft()))
+                        {
+                            return new Rule(rule1.getLeft(),BELIEVES,new Rule(((Key)(rule1.getRight())).getQ(),SAID,
+                                    ((EncryptedMessage)(rule2.getRight())).getMessage()));
+                        }
+                        else if ((((Key) (rule1.getRight())).getP()).equals(rule1.getLeft()))
+                        {
+                            return new Rule(rule1.getLeft(),BELIEVES,new Rule(((Key)(rule1.getRight())).getP(),SAID,
+                                    ((EncryptedMessage)(rule2.getRight())).getMessage()));
+                        }
                     }
                 }
             }
@@ -46,8 +57,60 @@ public class RuleBuilder {
                 return new Rule(rule1.getLeft(), BELIEVES, new Rule(((Rule)rule2.getRight()).getLeft(), BELIEVES, rule1.getRight()));
             }
 
+            //P believes public Q, P sees {X}_private_K => P believes Q said X
+
+            if ((rule1.getAction().equals(BELIEVES))&& rule2.getAction().equals(SEES))
+            {
+                if(rule1.getRight().getType().equals(BanObjectType.KEY)
+                        && ((Key) (rule1.getRight())).getKeyType().equals(PUBLIC_KEY)
+                        && (rule2.getType().equals(ENCRYPTED_MESSAGE))
+                        && ((EncryptedMessage)(rule1.getRight())).getKey().getKeyType().equals(PRIVATE_KEY))
+                    {
+                            if(((Key)(rule1.getRight())).getQ()!=null)
+                            {
+                                return new Rule(rule1.getLeft(),BELIEVES,new Rule(((Key)(rule1.getRight())).getQ(),SAID,
+                                        ((EncryptedMessage)(rule2.getRight())).getMessage()));
+                            }
+                            if(((Key)(rule1.getRight())).getP()!=null)
+                            {
+                                return new Rule(rule1.getLeft(),BELIEVES,new Rule(((Key)(rule1.getRight())).getP(),SAID,
+                                        ((EncryptedMessage)(rule2.getRight())).getMessage()));
+                            }
+                    }
+            }
+
+            //Jurisdiction
+            //P believes Q controls X, p believes Q believes X=> P believes X
+            if (rule1.getAction().equals(BELIEVES) && rule2.getAction().equals(BELIEVES))
+            {
+                if(rule1.getRight().getType().equals(BanObjectType.RULE)
+                        && rule2.getRight().getType().equals(BanObjectType.RULE))
+                {
+                    Rule rightRule1=(Rule)rule1.getRight();
+                    Rule rightRule2=(Rule)rule2.getRight();
+                    if(rightRule1.getLeft().equals(rightRule2.getLeft()))
+                    {
+                        if(rightRule1.getAction().equals(CONTROLS)&& (rightRule2).getAction().equals(BELIEVES))
+                        {
+                            if (rightRule1.getRight().getType().equals(rightRule2.getRight().getType()))
+                            {
+                                if(rightRule1.getRight().equals(rightRule2.getRight()))
+                                {
+                                return new Rule(rule1.getLeft(),BELIEVES,rightRule1.getRight());
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
 
         }
+        // Teoretic s-ar putea facea regula cu compuneri, discutabil
+
+
+
+
         return null;
     }
 }
