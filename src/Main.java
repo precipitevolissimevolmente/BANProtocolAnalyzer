e73305ba7024bb98eff76968a68d87f0198c2ac6
precipitevolissimevolmente <necunoscut23@gmail.com>
2014-04-03 13:04:00 +0300

    libs

--- src/Main.java
+++ src/Main.java
@@ -11,8 +11,14 @@ import static ban.ActionType.*;
 public class Main {
     public static void main(String[] args) {
         JSONBuilder kerberos = new JSONBuilder("/resources/Kerberos.json", "/resources/KerberosAssumption.json");
-        List<Rule> idealisedRules = kerberos.getIdealisedRules();
-        List<Rule> assumptions = kerberos.getAssumptions();
+//        List<Rule> idealisedRules = kerberos.getIdealisedRules();
+//        List<Rule> assumptions = kerberos.getAssumptions();
+
+        JSONBuilder nssk = new JSONBuilder("/resources/Needham-Schroeder-Shared-keys.json", "/resources/NSSKAssumptions.json");
+        List<Rule> idealisedRules=nssk.getIdealisedRules();
+        List<Rule> assumptions=nssk.getAssumptions();
+
+
         //RuleContainer.RULES.addAll(idealisedRules);
         RuleContainer.RULES.addAll(assumptions);
         System.out.println(idealisedRules.size());
@@ -26,13 +32,9 @@ public class Main {
                 System.out.println(bo.toString());
 
             }
-            System.out.println(RuleContainer.RULES.size());
-            System.out.println("--------------");
 
         }
-        JSONBuilder nssk = new JSONBuilder("/resources/Needham-Schroeder-Shared-keys.json", "/resources/NSSKAssumptions.json");
-//        RuleContainer.RULES.addAll(nssk.getIdealisedRules());
-//        RuleContainer.RULES.addAll(nssk.getAssumptions());
+        System.out.println(RuleContainer.RULES.size());
 
        /* boolean fresh = true;
         Message messageX = new Message(fresh);
