53a7aa461aacdc0447a77a6c6409bbeed3e93f2d
precipitevolissimevolmente <necunoscut23@gmail.com>
2014-04-16 20:17:11 +0300

    libs

--- src/Main.java
+++ src/Main.java
@@ -10,13 +10,13 @@ import static ban.ActionType.*;
 
 public class Main {
     public static void main(String[] args) {
-            JSONBuilder kerberos = new JSONBuilder("/resources/Kerberos.json", "/resources/KerberosAssumption.json");
-            List<Rule> idealisedRules = kerberos.getIdealisedRules();
-            List<Rule> assumptions = kerberos.getAssumptions();
-//        JSONBuilder nssk = new JSONBuilder("/resources/Needham-Schroeder-Shared-keys.json", "/resources/NSSKAssumptions.json");
-//
-//        List<Rule> idealisedRules = nssk.getIdealisedRules();
-//        List<Rule> assumptions = nssk.getAssumptions();
+//            JSONBuilder kerberos = new JSONBuilder("/resources/Kerberos.json", "/resources/KerberosAssumption.json");
+//            List<Rule> idealisedRules = kerberos.getIdealisedRules();
+//            List<Rule> assumptions = kerberos.getAssumptions();
+        JSONBuilder nssk = new JSONBuilder("/resources/Needham-Schroeder-Shared-keys.json", "/resources/NSSKAssumptions.json");
+
+        List<Rule> idealisedRules = nssk.getIdealisedRules();
+        List<Rule> assumptions = nssk.getAssumptions();
 
 
         RuleContainer.RULES.addAll(assumptions);
